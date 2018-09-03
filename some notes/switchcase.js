3dfe2d7cc69d488aac5d137ef70a95ea1e8084e8
TalaatMagdy <talaatmagdy75@gmail.com>
2018-09-03 20:52:42 +0200

    some notes

---
+++ some notes/switchcase.js
@@ -0,0 +1,21 @@
+var option = 6
+switch (option) {
+    case 1:
+        console.log("1");
+    case 2:
+        console.log("2");
+    case 3:
+        console.log("3");
+        break; //print only
+    case 4:
+        console.log("4");
+    case 5:
+        console.log("5");
+    case 6:
+        console.log("6");
+    case 7:
+        console.log("7");
+        //break;
+    default :
+        console.log("default")
+}
